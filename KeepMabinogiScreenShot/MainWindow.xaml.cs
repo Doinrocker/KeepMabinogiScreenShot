7857253aa00fb6e09b42d3492dff092406dd4e72
SangHyuk Kim <shkim0008@gmail.com>
2020-05-04 21:29:33 +0900

    Initial setup
    Signed-off-by: SangHyuk Kim <shkim0008@gmail.com>

---
+++ KeepMabinogiScreenShot/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿// Copyright by SangHyuk Kim
+// All Rights Reserved.
+//
+// Author: SangHyuk Kim, shkim0008@gmail.com
+
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.ComponentModel;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace KeepMabinogiScreenShot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public struct ButtonConfiguration
+        {
+            public Brush BGColor;
+            public Brush FGColor;
+
+            public ButtonConfiguration(Brush bgColor, Brush fgColor)
+            {
+                this.BGColor = bgColor;
+                this.FGColor = fgColor;
+            }
+        }
+        const string SetupFileName = "setup.json";
+        private string targetPath;
+        private string targetPattern;
+        private readonly Processor processor;
+        private readonly BackgroundWorker backupScreenShotMonitor;
+        private readonly ButtonConfiguration runBtnDefaultConfig = new ButtonConfiguration(Brushes.White, Brushes.Black);
+        private readonly ButtonConfiguration runBtnWorkingConfig = new ButtonConfiguration(Brushes.Crimson, Brushes.White);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.GetSetupFromJson();
+            this.processor = new Processor(this.targetPath, this.targetPattern);
+            this.backupScreenShotMonitor = new BackgroundWorker
+            {
+                WorkerSupportsCancellation = true
+            };
+            this.backupScreenShotMonitor.DoWork += this.monitoring_run;
+            this.Topmost = true;
+        }
+
+        private void GetSetupFromJson()
+        {
+            string setupFileContents;
+            using (StreamReader sw = new StreamReader(SetupFileName))
+            {
+                setupFileContents = sw.ReadToEnd();
+            }
+            var jsonReadObj = JObject.Parse(setupFileContents);
+            var targetPath = jsonReadObj.SelectToken("target_path").ToString();
+            targetPath = Environment.ExpandEnvironmentVariables(targetPath);
+            if (System.IO.Directory.Exists(targetPath))
+            {
+                this.targetPath = targetPath;
+            }
+            else
+            {
+                throw new System.ArgumentException("Invalid target path from setup.json");
+            }
+
+            this.targetPattern = jsonReadObj.SelectToken("target_pattern").ToString();
+
+            if (string.IsNullOrEmpty(this.targetPattern))
+            {
+                throw new System.ArgumentException("Invalid target pattern from setup.json");
+            }
+        }
+
+        private void runButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!this.backupScreenShotMonitor.IsBusy)
+            {
+                this.processor.BackupScreenShots();
+                this.backupScreenShotMonitor.RunWorkerAsync();
+                this.runButton.Background = this.runBtnWorkingConfig.BGColor;
+                this.runButton.Foreground = this.runBtnWorkingConfig.FGColor;
+                var flashButton = FindResource("FlashButton") as Storyboard;
+                flashButton.Begin();
+            }
+        }
+
+        private void forceStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.processor.ForceStop();
+            this.backupScreenShotMonitor.CancelAsync();
+            this.runButton.Background = this.runBtnDefaultConfig.BGColor;
+            this.runButton.Foreground = this.runBtnDefaultConfig.FGColor;
+        }
+
+        private void mvBackupFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.processor.MoveBackupFiles();
+        }
+
+        private void monitoring_run(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (this.processor.WorkComplete)
+                {
+                    this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, (ThreadStart)delegate ()
+                    { 
+                        var flashButton = FindResource("FlashButton") as Storyboard;
+                        flashButton.Stop();
+                        this.runButton.Background = this.runBtnDefaultConfig.BGColor;
+                        this.runButton.Foreground = this.runBtnDefaultConfig.FGColor;
+                    });
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ KeepMabinogiScreenShot/Processor.cs
@@ -0,0 +1,120 @@
+﻿// Copyright by SangHyuk Kim
+// All Rights Reserved.
+//
+// Author: SangHyuk Kim, shkim0008@gmail.com
+
+using System;
+using System.IO;
+using System.Linq;
+using System.ComponentModel;
+using System.Threading;
+
+namespace KeepMabinogiScreenShot
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Processor
+    {
+        const int NumDefaultScreenShots = 6;
+        const int DelayTimeMs = 10;
+        readonly string targetPath;
+        readonly string backupPath;
+        readonly string backupPattern;
+        private readonly BackgroundWorker workerObj;
+
+        public bool WorkComplete { get; private set; } = false;
+
+        public Processor(string targetPath, string backupPattern)
+        {
+            this.targetPath = targetPath;
+            this.backupPattern = backupPattern;
+            this.backupPath = $"{targetPath}backup/";
+            this.workerObj = new BackgroundWorker
+            {
+                WorkerSupportsCancellation = true
+            };
+            this.workerObj.DoWork += this.BackupScreenShots;
+            this.workerObj.RunWorkerCompleted += this.BackuptScreenShotDone;
+        }
+
+        public void ForceStop()
+        {
+            if (this.workerObj.IsBusy)
+            {
+                this.workerObj.CancelAsync();
+            }
+        }
+
+        public void MoveBackupFiles()
+        {
+            var now = DateTime.Now.ToString("yyyy_MM_dd_");
+            var screenShotNamePattern = now + '*';
+            string[] screenShotFileList = Directory.GetFiles(this.targetPath, screenShotNamePattern);
+            int startNumber = 0;
+            if (screenShotFileList.Length > 0)
+            {
+                string lastFileName = screenShotFileList[screenShotFileList.Length - 1];
+                string[] seperateName = lastFileName.Split('_');
+                int lastNumber = int.Parse(seperateName[seperateName.Length - 1].Split('.')[0]);
+                startNumber = lastNumber + 1;
+            }
+
+            string[] backupFileList = Directory.GetFiles($"{this.backupPath}", this.backupPattern);
+            if (backupFileList.Length < 1)
+            {
+                return;
+            }
+            else
+            {
+                foreach (var file in backupFileList.Select((name, index) => new { index, name }))
+                {
+                    var name = file.name;
+                    var number = startNumber + file.index;
+                    var destFileName = $"{this.targetPath}/{now}{number:0000}.jpg";
+                    File.Move(name, destFileName);
+                }
+            }
+        }
+
+        public void BackupScreenShots()
+        {
+            if (!this.workerObj.IsBusy)
+            {
+                this.WorkComplete = false;
+                this.workerObj.RunWorkerAsync();
+            }
+        }
+
+        public void BackupScreenShots(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if ((sender as BackgroundWorker).CancellationPending == true)
+                {
+                    return;
+                }
+                else
+                {
+                    string[] files = Directory.GetFiles(this.targetPath, $"{this.backupPattern}*");
+                    if (files.Length > NumDefaultScreenShots - 1)
+                    {
+                        foreach (var targetName in files)
+                        {
+                            var fileName = Path.GetFileName(targetName);
+                            var destName = $"{this.backupPath}{fileName}";
+                            File.Copy(targetName, destName, true);
+                        }
+                        return;
+                    }
+                }
+                Thread.Sleep(DelayTimeMs);
+            }
+        }
+
+        public void BackuptScreenShotDone(object sendeer, RunWorkerCompletedEventArgs e)
+        {
+            this.WorkComplete = true;
+        }
+    }
+}
